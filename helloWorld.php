2ced5531ee5db1074e0b0c42a62ac2a04fef603c
ahm223 <47831880+ahm223@users.noreply.github.com>
2019-03-19 10:45:36 -0400

    Update helloWorld.php

--- helloWorld.php
+++ helloWorld.php
@@ -2,21 +2,6 @@
 //This is a first file!
 
 function hackerFunction() {
-	echo "Hacker Man 400";
+	echo "Sorry for deleting everything...";
 }
-
-
-function zaksnewFunction() {
-	echo 'I accidentally echoed stuff in Mr Emmells branch';
-}
-
-function Jfunction() 
-{
-	echo "greetings fellow students";
-}
-
-function zaksEpicFunction() {
-	echo 'Whats poppin my dudes?';
-}
-
 ?>
